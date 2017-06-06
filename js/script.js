ee8aa5da43d3cbb1eef872c662a3f3957911e3c0
Gitu Mbugua <gmbugua38@gmail.com>
2017-06-06 16:01:52 +0300

    add second spec to js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ $(document).ready(function() {
 var leapYear = function(year) {
   return false;
 };
+
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
